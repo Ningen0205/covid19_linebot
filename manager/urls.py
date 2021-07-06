fb74f8b70ba68b19a1f45b8ad94adef956b8d6fe
hidaka <hidaka>
2021-07-06 13:34:13 +0900

    使用されていない関数を削除

--- manager/urls.py
+++ manager/urls.py
@@ -4,8 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('scraping',views.scraping,name='scraping'),
-    path('test',views.test,name='test'),
     path('webhook',views.webhook,name='webhook'),
-    path('init_database',views.init_database,name='init_database')
+
 ]
