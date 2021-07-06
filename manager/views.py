e9b505f7141b63ba65433eba135626cb1089f143
hidaka <hidaka>
2021-07-06 14:17:54 +0900

    スクリプト化している部分を削除

--- manager/views.py
+++ manager/views.py
@@ -32,24 +32,6 @@ CHANNEL_SECRET = os.environ.get('CHANNEL_SECRET')
 line_bot_api = LineBotApi(ACCESS_TOKEN)
 handler = WebhookHandler(CHANNEL_SECRET)
 
-# Create your views here.
-def index(request):
-    infections = infection.objects.all().order_by('id')
-    
-
-    t = infection.objects.order_by('id').reverse()
-    t = t[:47]
-    message = f'{t[46].date_string}時点の感染者数を報告します。 \n'
-
-    sum_infection = 0
-    for i in range(46,-1,-1):
-        message += f'{t[i].prefecture.name}:{t[i].infection} \n'
-        sum_infection += t[i].infection
-    # print(t[1].infection)
-    
-    message += f'本日の合計感染者数は{sum_infection}人でした。'
-    line_bot_api.broadcast(TextSendMessage(text=message))
-    return HttpResponse('メッセージ送信したよ')
 
 def webhook(request):
     # signatureの取得
@@ -69,18 +51,3 @@ def handle_message(event):
         event.reply_token,
         TextSendMessage(text=event.message.text)
     )
-
-
-def init_database(request):
-    prefecture_list = ["北海道","青森県","岩手県","宮城県","秋田県","山形県","福島県",
-"茨城県","栃木県","群馬県","埼玉県","千葉県","東京都","神奈川県",
-"新潟県","富山県","石川県","福井県","山梨県","長野県","岐阜県",
-"静岡県","愛知県","三重県","滋賀県","京都府","大阪府","兵庫県",
-"奈良県","和歌山県","鳥取県","島根県","岡山県","広島県","山口県",
-"徳島県","香川県","愛媛県","高知県","福岡県","佐賀県","長崎県",
-"熊本県","大分県","宮崎県","鹿児島県","沖縄県"
-    ] 
-    for i in range(0,47,1):
-        prefecture.objects.get_or_create(name=prefecture_list[i])
-    
-    return HttpResponse('初期化したよ')
