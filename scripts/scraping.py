742e7e7e5ff1aba924c59934347411a50e571a07
hidaka <hidaka>
2021-07-06 14:00:38 +0900

    インデント

--- scripts/scraping.py
+++ scripts/scraping.py
@@ -12,40 +12,40 @@ import json
 from manager.models import infection, prefecture
 
 def get_scraping():
-  # ドライバーをUIなしで使用するための設定
-  options = webdriver.ChromeOptions()
-  options.add_argument('--headless')
-  options.add_argument('--no-sandbox')
-  options.add_argument('--disable-gpu')
-  options.add_argument('--disable-dev-shm-usage')
-  options.add_argument('--log-level=0')
-  driver = webdriver.Chrome('chromedriver',options=options)
-  driver.implicitly_wait(10)
-  url = 'https://mainichi.jp/covid19'
+    # ドライバーをUIなしで使用するための設定
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-gpu')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--log-level=0')
+    driver = webdriver.Chrome('chromedriver',options=options)
+    driver.implicitly_wait(10)
+    url = 'https://mainichi.jp/covid19'
 
-  driver.get(url)
-  html = driver.page_source.encode('utf-8')
-  soup = BeautifulSoup(html, 'html.parser')
+    driver.get(url)
+    html = driver.page_source.encode('utf-8')
+    soup = BeautifulSoup(html, 'html.parser')
 
-  # 更新日時部分をスクレイピングしてる
-  data = soup.find('g', { 'id' : 'mc1'}).text
-  updated_time = soup.findAll('span', { 'class' : 'title-block-note'})[1].text
+    # 更新日時部分をスクレイピングしてる
+    data = soup.find('g', { 'id' : 'mc1'}).text
+    updated_time = soup.findAll('span', { 'class' : 'title-block-note'})[1].text
 
-  # 変数の初期化
-  result = cl.OrderedDict()
-  result["updated_time"] = updated_time
-  result["masculine_people"] = cl.OrderedDict()
+    # 変数の初期化
+    result = cl.OrderedDict()
+    result["updated_time"] = updated_time
+    result["masculine_people"] = cl.OrderedDict()
 
   # 実際にデータを取得している
-  for i in range(1,48,1):
-    prefecture_data = soup.find('g', { 'id' : f'mc{i}'}).text.split()
-    
-    prefecture = prefecture_data[0]
-    prefecture_num = int(prefecture_data[1])
+    for i in range(1,48,1):
+        prefecture_data = soup.find('g', { 'id' : f'mc{i}'}).text.split()
+        
+        prefecture = prefecture_data[0]
+        prefecture_num = int(prefecture_data[1])
 
-    result['masculine_people'][prefecture] = prefecture_num
-  
-  return result
+        result['masculine_people'][prefecture] = prefecture_num
+    
+    return result
 
 def run():
     r = get_scraping()
