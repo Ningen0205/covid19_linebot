7c02d5f60ca60266354d4316258fcb2b49fa7501
hidaka <hidaka>
2021-07-06 16:06:16 +0900

    オウム返しではないように変更

--- manager/views.py
+++ manager/views.py
@@ -53,12 +53,12 @@ def webhook(request):
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    i = prefecture.objects.filter(name="").first()
+    i = prefecture.objects.filter(name=event.message.text).first()
     if i == None:
         reply_text = "都道府県入れてね"
     else:
         reply_text =  f'{i.id()}番目の都道府県だよ'
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text)
+        TextSendMessage(text=reply_text)
     )
