c1aa17b1a5d1751291b39a71cd9281bb11c1aae3
hidaka <hidaka>
2021-07-08 11:50:00 +0900

    全国　と記入した場合、都道府県の情報を送信する

--- manager/views.py
+++ manager/views.py
@@ -49,15 +49,30 @@ def webhook(request):
 # テキストメッセージが送信された時のハンドルイベント
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    prefecture_obj = prefecture.objects.filter(name=event.message.text).first()
-    if prefecture_obj == None:
-        reply_text = "都道府県を正式名称で入れてください。"
+    if event.message.text == '日本':
+
+        infections = infection.objects.order_by('date').reverse()
+        infections = infections[:47]
+        
+        reply_text = f'{infections[46].date_string}時点の感染者数を報告します。 \n'
+
+        sum_infection = 0
+        for i in range(46,-1,-1):
+            reply_text += f'{infections[i].prefecture.name}:{infections[i].infection} \n'
+            sum_infection += infections[i].infection
+        
+        reply_text += f'本日の合計感染者数は{sum_infection}人でした。'
+        
     else:
-        latest_infection = infection.objects.filter(prefecture=prefecture_obj).order_by('date').last()
-        if latest_infection == None:
-            reply_text = '感染者情報が取得できませんでした。'
+        prefecture_obj = prefecture.objects.filter(name=event.message.text).first()
+        if prefecture_obj == None:
+            reply_text = "都道府県を正式名称で入れてください。"
         else:
-            reply_text = f'{prefecture_obj.name}の感染者数は、{latest_infection.date_string}で、{latest_infection.infection}人です。'
+            latest_infection = infection.objects.filter(prefecture=prefecture_obj).order_by('date').last()
+            if latest_infection == None:
+                reply_text = '感染者情報が取得できませんでした。'
+            else:
+                reply_text = f'{prefecture_obj.name}の感染者数は、{latest_infection.date_string}で、{latest_infection.infection}人です。'
 
     line_bot_api.reply_message(
         event.reply_token,
