d5dbf815321cdf3e8c8fc5917426970d60acc3dc
hidaka <hidaka>
2021-07-09 11:23:02 +0900

    pull

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-# 使用方法
+# 初期のセットアップ
+※秘匿情報の登録は省略しています。
 
 git clone https://github.com/Ningen0205/covid19_linebot.git  
 pip install -r requirements.txt  
@@ -10,3 +11,11 @@ python manage.py runserver
 
 # 概要
 [毎日新聞社](https://mainichi.jp/covid19)のサイトにある1日における感染者数を取得して返信するラインボットです。
+
+
+プラットフォームとしてHerokuを使用し、Python(Django)でスクレイピングし、Lineメッセージとして送信する機能があるリポジトリです。
+
+# 利用方法
+秘匿情報の登録(.envに記載か環境変数に登録)や、データベースのマイグレーションを完了していることが前提です。  
+python manage.py runscript scraping を実行すると、毎日新聞社のコロナ情報のページから情報をスクレイピングしてDBに格納します。  
+python manage.py runscript send_message　を実行するとLineメッセージとして、取得している最新の感染者数を全員に送信します。  
--- manager/views.py
+++ manager/views.py
@@ -2,15 +2,13 @@
 from django.shortcuts import render
 from django.http import HttpResponseForbidden,HttpResponse
 from .models import infection,prefecture
-from .scraping import get_scraping
 
-# json
-import json
-from collections import OrderedDict
+#csrf無効化
+from django.views.decorators.csrf import csrf_exempt
 
 # linebot関連
 from linebot import LineBotApi,WebhookHandler
-from linebot.models import MessageEvent, TextMessage,TextSendMessage
+from linebot.models import MessageEvent,TextMessage,TextSendMessage,FollowEvent
 
 # .env関連
 import os
@@ -32,6 +30,7 @@ CHANNEL_SECRET = os.environ.get('CHANNEL_SECRET')
 line_bot_api = LineBotApi(ACCESS_TOKEN)
 handler = WebhookHandler(CHANNEL_SECRET)
 
+@csrf_exempt
 def webhook(request):
     # signatureの取得
     signature = request.META['HTTP_X_LINE_SIGNATURE']
@@ -44,9 +43,46 @@ def webhook(request):
         print("Invalid signature. Please check your channel access token/channel secret.")
         return HttpResponseForbidden()
 
+    return HttpResponse('送信完了')
+
+
+# テキストメッセージが送信された時のハンドルイベント
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    if event.message.text == '全国':
+
+        infections = infection.objects.order_by('date').reverse()
+        infections = infections[:47]
+        
+        reply_text = f'{infections[46].date_string}時点の感染者数を報告します。 \n'
+
+        sum_infection = 0
+        for i in range(46,-1,-1):
+            reply_text += f'{infections[i].prefecture.name}:{infections[i].infection} \n'
+            sum_infection += infections[i].infection
+        
+        reply_text += f'本日の合計感染者数は{sum_infection}人でした。'
+
+    else:
+        prefecture_obj = prefecture.objects.filter(name=event.message.text).first()
+        if prefecture_obj == None:
+            reply_text = "都道府県を正式名称で入れてください。"
+        else:
+            latest_infection = infection.objects.filter(prefecture=prefecture_obj).order_by('date').last()
+            if latest_infection == None:
+                reply_text = '感染者情報が取得できませんでした。'
+            else:
+                reply_text = f'{prefecture_obj.name}の感染者数は、{latest_infection.date_string}で、{latest_infection.infection}人です。'
+
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text)
+        TextSendMessage(text=reply_text)
+    )
+
+# 友達追加した際のイベントを仮組で追加
+@handler.add(FollowEvent)
+def handle_follow(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text='追加してくれてありがとう!')
     )
