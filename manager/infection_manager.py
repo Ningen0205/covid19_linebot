d24528c82c919ce6a6837682998e2b5e4a50d3d2
hidaka <hidaka>
2021-07-13 14:01:44 +0900

    名前ではなく、objectを引数にするように変更

--- manager/infection_manager.py
+++ manager/infection_manager.py
@@ -1,8 +1,7 @@
 from django.db import models
 
 class infection_manager(models.Manager):
-    def latest_prefecture_data(self, prefecture_name):
-        prefecture_obj = super().get_queryset().filter(name=prefecture_name).first()
+    def latest_prefecture_data(self, prefecture_obj):
         return super().get_queryset().filter(prefecture=prefecture_obj).order_by('date').last()
     
     def latest_prefecture_all_data(self):
@@ -11,6 +10,6 @@ class infection_manager(models.Manager):
     def region_prefecture_data(self, region_array):
         result = []
         for p in region_array:
-            result.append(self.latest_prefecture_data(p.name))
+            result.append(self.latest_prefecture_data(p))
 
         return result
