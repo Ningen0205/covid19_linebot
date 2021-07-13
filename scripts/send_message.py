4b25527b99534889fe9940d9fd42875cfe8fea1d
hidaka <hidaka>
2021-07-13 15:50:57 +0900

    使用しないパッケージを削除

--- scripts/send_message.py
+++ scripts/send_message.py
@@ -1,18 +1,9 @@
 import os
 import sys
 
-# linebot関連
-from linebot import LineBotApi
-from linebot.models import TextSendMessage
-
-# django.models
-from manager.models import infection
-
 # message.pyをimportするために、親ディレクトリへ移動
 sys.path.append('../')
 from manager.message import send_message
-
-ACCESS_TOKEN = os.environ.get('ACCESS_TOKEN')
 
 def run():
     send_message()
